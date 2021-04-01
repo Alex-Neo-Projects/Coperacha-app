9d456e1a5bd485145dde3393f93e3ca21fc943c9
Alex Reyes <reyesruizalex@gmail.com>
2021-04-01 01:47:18 -0400

    React navigation mostly working

--- components/Manage.js
+++ components/Manage.js
@@ -1,13 +1,90 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { kit } from '../root'
+import {   
+  requestAccountAddress,
+  waitForAccountAuth,
+} from '@celo/dappkit';
+import * as Linking from 'expo-linking';
 
-function Manage() {
+class Manage extends React.Component {
+  // Set the defaults for the state
+  state = {
+    address: 'Not logged in',
+    phoneNumber: 'Not logged in',
+    cUSDBalance: 'Not logged in',
+    contractName: '',
+  }
+
+  login = async () => {
+    // A string you can pass to DAppKit, that you can use to listen to the response for that request
+    const requestId = 'login';
+    
+    // A string that will be displayed to the user, indicating the DApp requesting access/signature
+    const dappName = 'Coperacha';
+    
+    // The deeplink that the Celo Wallet will use to redirect the user back to the DApp with the appropriate payload.
+    const callback = Linking.makeUrl('/my/path');
   
-  return (
-    <View>
-      <Text>{"\n\n\n\n\n\n"}TODO: Manage page</Text>
-    </View>
-  );
+    // Ask the Celo Alfajores Wallet for user info
+    requestAccountAddress({
+      requestId,
+      dappName,
+      callback,
+    });
+  
+    // Wait for the Celo Wallet response
+    const dappkitResponse = await waitForAccountAuth(requestId);
+
+    // Set the default account to the account returned from the wallet
+    kit.defaultAccount = dappkitResponse.address;
+
+    // Get the stable token contract
+    const stableToken = await kit.contracts.getStableToken();
+
+    // Get the user account balance (cUSD)
+    const cUSDBalanceBig = await stableToken.balanceOf(kit.defaultAccount);
+    
+    // Convert from a big number to a string
+    let cUSDBalance = cUSDBalanceBig.toString();
+    
+    // Update state
+    this.setState({ cUSDBalance, 
+                    isLoadingBalance: false,
+                    address: dappkitResponse.address, 
+                    phoneNumber: dappkitResponse.phoneNumber })
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <Button title="Login" 
+          onPress={()=> this.login()} />
+
+        <Text style={styles.title}>Account Info:</Text>
+
+        <Text>Current Account Address:</Text>
+        <Text>{this.state.address}</Text>
+        <Text>Phone number: {this.state.phoneNumber}</Text>
+        <Text>cUSD Balance: {this.state.cUSDBalance}</Text>
+
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    marginVertical: 8, 
+    fontSize: 20, 
+    fontWeight: 'bold'
+  }
+});
 
 export default Manage;
