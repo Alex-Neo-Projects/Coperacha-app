92c7f787e6635fd94850c80050792e9ab00d93de
Alex Reyes <reyesruizalex@gmail.com>
2021-04-02 12:57:52 -0400

    create first file in components

---
+++ components/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+function LogIn() {
+  return(
+    <Button title="Log In"></Button>
+    // TODO!!!
+  );
+}
+export default LogIn;
